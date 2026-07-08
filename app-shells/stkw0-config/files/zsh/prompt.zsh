7baedb525c8ff1eadf64e4498e859c4d18ca21ce
David Roman <droman@ifae.es>
2026-07-08 11:16:52 +0200

    app-shells/stkw0-config: add 13

--- app-shells/stkw0-config/files/zsh/prompt.zsh
+++ app-shells/stkw0-config/files/zsh/prompt.zsh
@@ -25,7 +25,15 @@ else
 	SSH=""
 fi
 
-RPROMPT="${current_dir} ${git_info} ${return_code}"
+if [[ -n ${IN_NIX_SHELL} && ${IN_NIX_SHELL} != "0" || ${IN_NIX_RUN} && ${IN_NIX_RUN} != "0" ]]; then
+	if [[ -n ${IN_WHICH_NIX_SHELL} ]] then
+		NIX_SHELL_NAME=": ${IN_WHICH_NIX_SHELL}"
+	fi
+	NAME="nix"
+	NIX_PROMPT="%F{8}[%F{3}${NAME}${NIX_SHELL_NAME}%F{8}]%f"
+fi
+
+RPROMPT="${current_dir} ${git_info} ${NIX_PROMPT} ${return_code}"
 if [ "$EUID" = "0" ] || [ "$USER" = "root" ] ; then
 	PROMPT=$'\n$(zcock)'" ${SSH} %{$fg_bold[black]%}@%M%{$reset_color%} %(1j.[%j] .)%{$fg_bold[yellow]%}❱%{$reset_color%} "
 else
